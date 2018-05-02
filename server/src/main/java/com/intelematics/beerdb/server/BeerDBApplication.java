986608dadb3909802f6d6d3b3502507259f63d17
Felicity Hughes <Felicity.Hughes@intelematics.com>
2018-05-03 09:45:27 +1000

    First version as directed by tutorial at https://developer.okta.com/blog/2017/04/26/bootiful-development-with-spring-boot-and-angular

---
+++ server/src/main/java/com/intelematics/beerdb/server/BeerDBApplication.java
@@ -0,0 +1,12 @@
+package com.intelematics.beerdb.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerDBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerDBApplication.class, args);
+    }
+}
