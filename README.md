76d2f5103c222b97cd4b31cf824ec93c10ef88a0
Felicity Hughes <felicity.hughes@intelematics.com>
2019-11-07 17:42:20 +1100

    Rebuilt client with CSS styling.
    Previously regenerated with Stylus styling and stuff was broken.  All vulnerable dependencies now removed, styling fixed and README updated to explain how to run with Gradle instead of Maven (as used by the source tutorial).

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
  A simple Spring Boot server for managing an in-memory beer database.  Angular
  front end uses Giphy service to add some interesting pictures.
  
+ Run via `gradle bootRun` for the server and `ng serve` for the client.
+ 
  Original tutorial available at:  https://developer.okta.com/blog/2017/04/26/bootiful-development-with-spring-boot-and-angular
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
 
@@ -13,8 +12,7 @@ import { BeerListComponent } from './beer-list/beer-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
